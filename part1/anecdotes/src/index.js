232c669da3219910c5472afd294c574af95e1028
Sean <seantyu@utexas.edu>
2020-05-18 21:33:48 -0500

    finished part 1 exercises

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Anecdote = ({ anecdote, votes }) => {
+  return (
+    <>
+      <p>
+        {anecdote}
+        <br></br>
+        has {votes} votes
+      </p>
+    </>
+  )
+}
+
+const MostVotes = ({anecdotes, votes, max}) => {
+  // Find the idx of the anecdotes with the max votes
+  return (
+    <div>
+      <h1> Anecdote with the most votes </h1>
+      <Anecdote anecdote={anecdotes[max]} votes={votes[max]} />
+    </div>
+  )
+}
+
+const randomIdx = () => Math.floor(Math.random() * anecdotes.length) 
+
+const findMaxIdx = (array) => {
+    let max = array[0]
+    let maxIdx = 0
+
+    for(let i = 1; i < array.length; i++) {
+      if(array[i] > max) {
+        max = array[i]
+        maxIdx = i
+      }
+    }
+
+    return maxIdx
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, updateVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [mostVotes, updateMost] = useState(0)
+
+
+  const handleVote = (idx) => {
+    const copy = [ ...votes ]
+    copy[idx] += 1
+    updateVotes(copy)
+    updateMost(findMaxIdx(votes))
+  }
+
+  return (
+    <div>
+      <h1> Anecdote of the day </h1>
+      <Anecdote anecdote={props.anecdotes[selected]} votes={votes[selected]} />
+      <Button onClick={() => handleVote(selected)} text="vote" />
+      <Button onClick={() => setSelected(randomIdx())} text="next anecdote" />
+      <MostVotes anecdotes={props.anecdotes} votes={votes} max={mostVotes}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,73 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistic = ({ label, value, end}) => {
+  if (end) {
+    return (
+      <tr>
+        <td>{label}</td>
+        <td>{value}</td>
+        <td>{end}</td>
+      </tr>
+    )
+  } else{
+    return (
+      <tr>
+        <td>{label}</td>
+        <td>{value}</td>
+      </tr>
+    ) 
+  }
+} 
+
+
+
+const Statistics = ({ good, bad, neutral }) => {
+  let sum = good + bad + neutral
+
+  const calculateAverage = (good, neutral, bad) => {
+    let score = good - bad
+    if (score == 0) {
+      return 0
+    } else {
+      return score / sum
+    }
+  }
+
+  const calculatePositive = (good, neutral, bad) => {
+    if (sum == 0) {
+      return 0
+    } else {
+      return (good / sum) * 100
+    }
+  }
+
+  if (sum == 0) {
+    return <div>No feedback given</div>
+  }
+
+  return (
+    <table>
+      <tbody>
+        <Statistic label={"good"} value={good} />
+        <Statistic label={"neutral"} value={neutral} />
+        <Statistic label={"bad"} value={bad} />
+        <Statistic label={"all"} value={sum} />
+        <Statistic label={"average"} value={calculateAverage(good, neutral, bad)} />
+        <Statistic label={"positive"} value={calculatePositive(good, neutral, bad)} end={"%"} /> 
+      </tbody>
+    </table>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -10,10 +77,12 @@ const App = () => {
   return (
     <div>
       <h1> Give Feedback </h1>
-      // buttons go here
+      <Button onClick={() => setGood(good + 1)} text={"good"} />
+      <Button onClick={() => setNeutral(neutral+ 1)} text={"neutral"} />
+      <Button onClick={() => setBad(bad + 1)} text={"bad"} />
       <h1> Statistics </h1>
-      // display info here
-    </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+   </div>
   )
 }
 
